25dc1ef79a647ecc1fd494ee2db1816bd8d9fb09
Henrique Faria <rique.far.rib@gmail.com>
2023-07-07 11:35:19 -0300

    Program now receive host from args, new option to check list of ports

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strconv"
 	"sort"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -34,17 +35,55 @@ func create_array_from_range(start int, end int) []int {
 	return array
 }
 
+func get_ports_list_from_args() []int {
+	var portsArray []int
+	var portsStringArray []string = strings.Split(os.Args[2], ",")
+	portsArray = make([]int, len(portsStringArray))
+	for i, p := range portsStringArray {
+		portsArray[i], _ = strconv.Atoi(p)
+	}
+	return portsArray
+}
+
+func print_usage_and_exit() {
+	fmt.Println("Usage: program [options] host")
+	os.Exit(1)
+}
+
 func main() {
-	fmt.Println(len(os.Args))
+	var numArgs = len(os.Args)
+
+	var host string
+
+	//TODO 	verify num threads if num ports is less than num threads,
+	// 		check if it is negative or 0
+	//TODO 	check timeout, if server do not exist or is not reacheable
+	// 		it takes too long
+	var numThreads int = 1
+	var maxNumPorts int = 65535 // 1 - 65535
+
+	var lastArg int = 1
+
+	var portsArray []int
+
+	if numArgs > 1 {
+		if os.Args[1] == "-p" {
+			if numArgs != 4 {
+				print_usage_and_exit()
+			}
+			lastArg = 3
+			portsArray = get_ports_list_from_args()
+			host = os.Args[lastArg]
+		} else {
+			host = os.Args[lastArg]
+			portsArray = create_array_from_range(1, maxNumPorts + 1)
+		}
+	} else {
+		print_usage_and_exit()
+	}
 
 	var wg sync.WaitGroup
 
-	var host string = "127.0.0.1"
-
-	var numThreads int = 20
-	var maxNumPorts int = 65535 // 1 - 65535
-
-	var portsArray []int = create_array_from_range(1, maxNumPorts + 1)
 	var numPorts int = len(portsArray)
 	var portsPerThread int = numPorts/numThreads
 
